4487d1954f092a16b4649dab7716cdf48053d636
Mohamad Hitami <mohamad.hitami@wgs-id.com>
2022-09-05 14:43:09 +0700

    make it better

--- account/forms.py
+++ account/forms.py
@@ -5,12 +5,10 @@ from .models import *
 class CreateUserForm(UserCreationForm):
 	class Meta:
 		model = User
-		fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+		fields = ['username', 'email', 'password1', 'password2']
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.fields['username'].widget.attrs.update({ 'class': 'form-control','type': 'text','placeholder':'username..' })
 		self.fields['email'].widget.attrs.update({ 'class': 'form-control','type':'email', 'placeholder':'Email..' })
-		self.fields['first_name'].widget.attrs.update({ 'class': 'form-control', 'type':'text', 'placeholder':'Nama Depan..' })
-		self.fields['last_name'].widget.attrs.update({ 'class': 'form-control', 'type':'text', 'placeholder':'Nama Belakang..' })
 		self.fields['password1'].widget.attrs.update({ 'class': 'form-control', 'type':'password' , 'placeholder':'Password..' })
 		self.fields['password2'].widget.attrs.update({ 'class': 'form-control','type':'password' , 'placeholder':'Re-Password..' })

41f57f676bdb91d375cda021659754604e1fa1bb
Mohamad Hitami <mohamad.hitami@wgs-id.com>
2022-09-18 14:48:55 +0700

    update ui

--- pos/settings.py
+++ pos/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 # DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
 
-# ALLOWED_HOSTS = ['repositoryfti.herokuapp.com']
+ALLOWED_HOSTS = ['repositoryfti.herokuapp.com']
 # ALLOWED_HOSTS = ['*']
 
 

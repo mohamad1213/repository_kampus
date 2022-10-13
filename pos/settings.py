f3b08646ca90618e61986117a02e829e7aefb7ea
Mohamad Hitami <mohamad.hitami@wgs-id.com>
2022-10-13 14:21:01 +0700

    update

--- pos/settings.py
+++ pos/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -151,10 +152,10 @@ USE_L10N = True
 USE_TZ = True
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS =[
-#     os.path.join(BASE_DIR, 'static') 
-# ]
-STATIC_ROOT =  os.path.join(BASE_DIR, 'static') 
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, 'static') 
+]
+STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles') 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
 
